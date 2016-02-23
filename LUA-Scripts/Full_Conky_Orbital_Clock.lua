35d6e548fd08de64e169ea0765f7cdf2accc611f
Keaton <linux.keaton@gmail.com>
2016-02-23 08:56:35 -0600

    Update Full_Conky_Orbital_Clock.lua
    Noticed all instances of blue and green colors were switched- corrected, and changed all variables to local.

--- LUA-Scripts/Full_Conky_Orbital_Clock.lua
+++ LUA-Scripts/Full_Conky_Orbital_Clock.lua
@@ -21,87 +21,87 @@ function conky_OrbitalClock()
 		sm=h
 	end
 	---------------------Color Settings
-	SunRed=256/256
-	SunGreen=0/256
-	SunBlue=192/256
-	SunAlpha=192/256
+	local SunRed=256/256
+	local SunGreen=192/256
+	local SunBlue=0/256
+	local SunAlpha=192/256
 	----------------
-	HourRed=256/256
-	HourGreen=256/256
-	HourBlue=256/256
-	HourAlpha=192/256
+	local HourRed=256/256
+	local HourGreen=256/256
+	local HourBlue=256/256
+	local HourAlpha=192/256
 	----------------
-	MinRed=0/256
-	MinGreen=256/256
-	MinBlue=0/256
-	MinAlpha=192/256
+	local MinRed=0/256
+	local MinGreen=256/256
+	local MinBlue=0/256
+	local MinAlpha=192/256
 	----------------
-	SecRed=256/256
-	SecGreen=0/256
-	SecBlue=0/256
-	SecAlpha=192/256
+	local SecRed=256/256
+	local SecGreen=0/256
+	local SecBlue=0/256
+	local SecAlpha=192/256
 	----------------
-	LineAlpha=128/256
+	local LineAlpha=128/256
 	---------------------Celestial Body Settings
-	SunR=sm/11
-	SunPosX=w/2
-	SunPosY=h/2
-	PlanetR=sm/26
-	SecR=sm/6
-	MinR=(2/7)*sm
-	HouR=(3/7)*sm
+	local SunR=sm/11
+	local SunPosX=w/2
+	local SunPosY=h/2
+	local PlanetR=sm/26
+	local SecR=sm/6
+	local MinR=(2/7)*sm
+	local HouR=(3/7)*sm
 	---------------------Time Settings
-	Hours,Mins,Secs=tonumber(os.date("%l")),tonumber(os.date("%M")),tonumber(os.date("%S"))
+	local Hours,Mins,Secs=tonumber(os.date("%l")),tonumber(os.date("%M")),tonumber(os.date("%S"))
 	---------------------Orbit Calculations
-	SecPosX=SunPosX-math.sin(-Secs/60*2*math.pi)*SecR
-	SecPosY=SunPosY-math.cos(-Secs/60*2*math.pi)*SecR
-	MinPosX=SunPosX-math.sin(-(Mins*60+Secs)/3600*2*math.pi)*MinR
-	MinPosY=SunPosY-math.cos(-(Mins*60+Secs)/3600*2*math.pi)*MinR
-	HourPosX=SunPosX-math.sin(-(Hours*60*60+Mins*60+Secs)/43200*2*math.pi)*HouR
-	HourPosY=SunPosY-math.cos(-(Hours*60*60+Mins*60+Secs)/43200*2*math.pi)*HouR
+	local SecPosX=SunPosX-math.sin(-Secs/60*2*math.pi)*SecR
+	local SecPosY=SunPosY-math.cos(-Secs/60*2*math.pi)*SecR
+	local MinPosX=SunPosX-math.sin(-(Mins*60+Secs)/3600*2*math.pi)*MinR
+	local MinPosY=SunPosY-math.cos(-(Mins*60+Secs)/3600*2*math.pi)*MinR
+	local HourPosX=SunPosX-math.sin(-(Hours*60*60+Mins*60+Secs)/43200*2*math.pi)*HouR
+	local HourPosY=SunPosY-math.cos(-(Hours*60*60+Mins*60+Secs)/43200*2*math.pi)*HouR
 	---------------------Creating Celestial Bodies
-	StartAngleS=(math.pi+Secs/60*2*math.pi)
-	EndAngleS=(2*math.pi+Secs/60*2*math.pi)
-	StartAngleM=(math.pi+(Mins*60+Secs)/3600*2*math.pi)
-	EndAngleM=(2*math.pi+(Mins*60+Secs)/3600*2*math.pi)
-	StartAngleH=(math.pi+(Hours*60*60+Mins*60+Secs)/43200*2*math.pi)
-	EndAngleH=(2*math.pi+(Hours*60*60+Mins*60+Secs)/43200*2*math.pi)
+	local StartAngleS=(math.pi+Secs/60*2*math.pi)
+	local EndAngleS=(2*math.pi+Secs/60*2*math.pi)
+	local StartAngleM=(math.pi+(Mins*60+Secs)/3600*2*math.pi)
+	local EndAngleM=(2*math.pi+(Mins*60+Secs)/3600*2*math.pi)
+	local StartAngleH=(math.pi+(Hours*60*60+Mins*60+Secs)/43200*2*math.pi)
+	local EndAngleH=(2*math.pi+(Hours*60*60+Mins*60+Secs)/43200*2*math.pi)
 	---------------
 	cairo_arc(cr,SunPosX,SunPosY,SunR,0,2*math.pi)
-	cairo_set_source_rgba(cr,SunRed,SunBlue,SunGreen,SunAlpha)
+	cairo_set_source_rgba(cr,SunRed,SunGreen,SunBlue,SunAlpha)
 	cairo_fill(cr)
 	---------------
 	cairo_arc(cr,SecPosX,SecPosY,PlanetR,0,2*math.pi)
-	cairo_set_source_rgba(cr,SecRed,SecBlue,SecGreen,SecAlpha)
+	cairo_set_source_rgba(cr,SecRed,SecGreen,SecBlue,SecAlpha)
 	cairo_fill(cr)
 	cairo_arc(cr,SecPosX,SecPosY,PlanetR,StartAngleS,EndAngleS)
 	cairo_set_source_rgba(cr,0,0,0,SecAlpha)
 	cairo_fill(cr)
 	---------------
 	cairo_arc(cr,MinPosX,MinPosY,PlanetR,0,2*math.pi)
-	cairo_set_source_rgba(cr,MinRed,MinBlue,MinGreen,MinAlpha)
+	cairo_set_source_rgba(cr,MinRed,MinGreen,MinBlue,MinAlpha)
 	cairo_fill(cr)
 	cairo_arc(cr,MinPosX,MinPosY,PlanetR,StartAngleM,EndAngleM)
 	cairo_set_source_rgba(cr,0,0,0,MinAlpha)
 	cairo_fill(cr)
 	---------------
 	cairo_arc(cr,HourPosX,HourPosY,PlanetR,0,2*math.pi)
-	cairo_set_source_rgba(cr,HourRed,HourBlue,HourGreen,HourAlpha)
+	cairo_set_source_rgba(cr,HourRed,HourGreen,HourBlue,HourAlpha)
 	cairo_fill(cr)
 	cairo_arc(cr,HourPosX,HourPosY,PlanetR,StartAngleH,EndAngleH)
 	cairo_set_source_rgba(cr,0,0,0,HourAlpha)
 	cairo_fill(cr)
 	---------------
 	cairo_arc(cr,SunPosX,SunPosY,SecR,0,2*math.pi)
-	cairo_set_source_rgba(cr,SecRed,SecBlue,SecGreen,LineAlpha)
+	cairo_set_source_rgba(cr,SecRed,SecGreen,SecBlue,LineAlpha)
 	cairo_stroke(cr)
 	---------------
 	cairo_arc(cr,SunPosX,SunPosY,MinR,0,2*math.pi)
-	cairo_set_source_rgba(cr,MinRed,MinBlue,MinGreen,LineAlpha)
+	cairo_set_source_rgba(cr,MinRed,MinGreen,MinBlue,LineAlpha)
 	cairo_stroke(cr)
 	---------------
 	cairo_arc(cr,SunPosX,SunPosY,HouR,0,2*math.pi)
-	cairo_set_source_rgba(cr,HourRed,HourBlue,HourGreen,LineAlpha)
+	cairo_set_source_rgba(cr,HourRed,HourGreen,HourBlue,LineAlpha)
 	cairo_stroke(cr)
 	---------------------Finishing up
 	cairo_destroy(cr)
